afc79e60d092d0522abc45efdfdc65ddefdc0b15
foo <foo@bar.de>
2020-06-18 08:54:19 +0200

    fix: optimization

--- src/rules/prefer-alias.js
+++ src/rules/prefer-alias.js
@@ -1,16 +1,23 @@
 import { OptionManager } from '@babel/core'
-import {
-  find,
-  findKey,
-  keys,
-  replace,
-  some,
-  startsWith,
-} from '@dword-design/functions'
+import { find, keys, replace, some, startsWith } from '@dword-design/functions'
 import { resolvePath as defaultResolvePath } from 'babel-plugin-module-resolver'
 import P from 'path'
 
 const isParentImport = path => /^(\.\/)?\.\.\//.test(path)
+const findMatchingAlias = (sourcePath, currentFile, options) => {
+  const resolvePath = options.resolvePath || defaultResolvePath
+  const absoluteSourcePath = P.resolve(P.dirname(currentFile), sourcePath)
+  for (const aliasName of options.alias |> keys) {
+    const path = P.resolve(
+      P.dirname(currentFile),
+      resolvePath(`${aliasName}/`, currentFile, options)
+    )
+    if (absoluteSourcePath |> startsWith(path)) {
+      return { name: aliasName, path }
+    }
+  }
+  return undefined
+}
 
 export default {
   meta: {
@@ -18,57 +25,47 @@ export default {
     fixable: true,
   },
   create: context => {
-    const path = context.getFilename()
-    const folder = P.dirname(path)
+    const currentFile = context.getFilename()
+    const folder = P.dirname(currentFile)
     // can't check a non-file
-    if (path === '<text>') return {}
+    if (currentFile === '<text>') return {}
     const manager = new OptionManager()
     const babelConfig = manager.init({
       babelrc: true,
       root: folder,
-      filename: path,
+      filename: currentFile,
     })
     const plugin = babelConfig.plugins |> find({ key: 'module-resolver' })
     const options = plugin.options
     const resolvePath = options.resolvePath || defaultResolvePath
     return {
       ImportDeclaration: node => {
-        const importPath = node.source.value
+        const sourcePath = node.source.value
         const hasAlias =
           options.alias
           |> keys
-          |> some(alias => importPath |> startsWith(`${alias}/`))
-        const importWithoutAlias = resolvePath(importPath, path, options)
+          |> some(alias => sourcePath |> startsWith(`${alias}/`))
         // relative parent
-        if (importPath |> isParentImport) {
-          const absoluteImportPath = P.resolve(folder, importPath)
-          const matchingAlias =
-            options.alias
-            |> findKey(
-              (aliasPath, alias) =>
-                absoluteImportPath
-                |> startsWith(
-                  P.resolve(folder, resolvePath(`${alias}/`, path, options))
-                )
-            )
+        if (sourcePath |> isParentImport) {
+          const matchingAlias = findMatchingAlias(
+            sourcePath,
+            currentFile,
+            options
+          )
           if (!matchingAlias) {
             return context.report({
               node,
-              message: `Unexpected parent import '${importPath}'. No matching alias found to fix the issue`,
+              message: `Unexpected parent import '${sourcePath}'. No matching alias found to fix the issue`,
             })
           }
-          const rewrittenImport = `${matchingAlias}/${
-            P.relative(
-              P.resolve(
-                folder,
-                resolvePath(`${matchingAlias}/`, path, options)
-              ),
-              absoluteImportPath
-            ) |> replace(/\\/g, '/')
+          const absoluteImportPath = P.resolve(folder, sourcePath)
+          const rewrittenImport = `${matchingAlias.name}/${
+            P.relative(matchingAlias.path, absoluteImportPath)
+            |> replace(/\\/g, '/')
           }`
           return context.report({
             node,
-            message: `Unexpected parent import '${importPath}'. Use '${rewrittenImport}' instead`,
+            message: `Unexpected parent import '${sourcePath}'. Use '${rewrittenImport}' instead`,
             fix: fixer =>
               fixer.replaceTextRange(
                 [node.source.range[0] + 1, node.source.range[1] - 1],
@@ -76,10 +73,11 @@ export default {
               ),
           })
         }
+        const importWithoutAlias = resolvePath(sourcePath, currentFile, options)
         if (!(importWithoutAlias |> isParentImport) && hasAlias) {
           return context.report({
             node,
-            message: `Unexpected subpath import via alias '${importPath}'. Use '${importWithoutAlias}' instead`,
+            message: `Unexpected subpath import via alias '${sourcePath}'. Use '${importWithoutAlias}' instead`,
             fix: fixer =>
               fixer.replaceTextRange(
                 [node.source.range[0] + 1, node.source.range[1] - 1],
